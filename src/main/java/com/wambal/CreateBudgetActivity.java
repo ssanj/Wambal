476f89e175251d197f54d8e8dc3640ef4d7770cc
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-05-14 16:39:49 +1000

    added create budget screen

---
+++ src/main/java/com/wambal/CreateBudgetActivity.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2009 Sanjiv Sahayam
+ * Licensed under the Apache License, Version 2.0
+ */
+package com.wambal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public final class CreateBudgetActivity extends Activity {
+
+    private static class CreatedBudget {
+        private String category;
+        private String name;
+        private float cost;
+        private boolean save;
+
+        @Override
+        public String toString() {
+            return new StringBuilder("Budget [V]").append("\n").
+                        append(" category:").append(category).append("\n").
+                        append(" name:").append(name).append("\n").
+                        append(" cost:").append(cost).append("\n").
+                        append(" save:").append(save).append("\n").toString();
+        }
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_budget);
+
+        final CreatedBudget createdBudget = new CreatedBudget();
+
+        Spinner category = findTypedViewById(R.id.budgetCategory);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.budget_categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        category.setAdapter(adapter);
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override public void onItemSelected(final AdapterView<?> adapterView, final View view, final int pos, final long id) {
+                createdBudget.category = adapterView.getItemAtPosition(pos).toString();
+            }
+
+            @Override public void onNothingSelected(final AdapterView<?> adapterView) { }
+        });
+
+        Button button = findTypedViewById(R.id.saveBudgetButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(final View view) {
+                EditText budgetName = findTypedViewById(R.id.budgetName);
+                createdBudget.name = budgetName.getText().toString();
+
+                EditText budgetCost = findTypedViewById(R.id.budgetCost);
+                createdBudget.cost = Float.parseFloat(budgetCost.getText().toString());
+
+                CheckBox saveBudget = findTypedViewById(R.id.saveBudget);
+                createdBudget.save = saveBudget.isChecked();
+
+                Toast.makeText(CreateBudgetActivity.this, createdBudget.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private <T> T findTypedViewById(int id) {
+        return (T) findViewById(id);
+    }
+}
--- src/main/java/com/wambal/WambalActivity.java
+++ src/main/java/com/wambal/WambalActivity.java
@@ -1,11 +1,11 @@
 package com.wambal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
 
 public class WambalActivity extends Activity {
 
@@ -19,7 +19,8 @@ public class WambalActivity extends Activity {
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override public void onItemClick(final AdapterView<?> adapterView, final View view, final int position, final long index) {
-                Toast.makeText(WambalActivity.this, "" + adapterView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(WambalActivity.this, CreateBudgetActivity.class);
+                startActivity(intent);
             }
         });
     }
