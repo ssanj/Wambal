32d985d2828207ae7ab7a0de682e72ecbd5c0be3
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-05-19 20:31:15 +1000

    added context menu

---
+++ copy_to_nginx.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp target/Wambal.apk ~/www/android/
--- src/main/java/com/wambal/ShoppingListActivity.java
+++ src/main/java/com/wambal/ShoppingListActivity.java
@@ -6,6 +6,11 @@ package com.wambal;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public final class ShoppingListActivity extends Activity {
 
@@ -13,4 +18,31 @@ public final class ShoppingListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shopping_list);
     }
+
+    @Override public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.shopping_list_menu, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.shopping_list_add_item: return toaster("Add Item");
+            case R.id.shopping_list_delete_item: return toaster("Delete Item");
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private boolean toaster(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+//    @Override public void onCreateContextMenu(final ContextMenu menu, final View v, final ContextMenu.ContextMenuInfo menuInfo) {
+//        super.onCreateContextMenu(menu, v, menuInfo);
+//        getMenuInflater().inflate(R.menu.shopping_list_menu, menu);
+//    }
+//
+//    @Override public boolean onContextItemSelected(final MenuItem item) {
+//        return onOptionsItemSelected(item);
+//    }
 }
