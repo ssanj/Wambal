3ec17c13d41a3cdd211a17ddc1af6d30efe75ac2
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-06-12 19:49:27 +1000

    updated to correctly include cursor data. need to format add button to appear on the same line as the text

--- src/main/java/com/wambal/activity/ManageCategoryActivity.java
+++ src/main/java/com/wambal/activity/ManageCategoryActivity.java
@@ -4,6 +4,7 @@
  */
 package com.wambal.activity;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
@@ -13,9 +14,11 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.HeaderViewListAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
@@ -24,35 +27,28 @@ import com.wambal.cp.WambalContentProvider;
 
 import java.util.ArrayList;
 
-public final class ManageCategoryActivity extends ListActivity {
+public final class ManageCategoryActivity extends Activity {
 
     private static final int CREATE_DIALOG = 1;
 
     @Override protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ArrayList<ListView.FixedViewInfo> footers = new ArrayList<ListView.FixedViewInfo>();
-        ArrayList<ListView.FixedViewInfo> headers = new ArrayList<ListView.FixedViewInfo>();
-
-        addHeaders(headers, getListView());
-        Cursor cursor = getContentResolver().query(WambalContentProvider.CONTENT_URI, new String[]{WambalContentProvider.Category._ID, WambalContentProvider.Category.NAME}, null, null, null);
-        startManagingCursor(cursor);
-
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor,
-                new String[]{WambalContentProvider.Category.NAME}, new int[]{android.R.id.text1});
-        HeaderViewListAdapter headerAdapter = new HeaderViewListAdapter(headers, footers, adapter);
-        setListAdapter(headerAdapter);
-    }
-
-    private void addHeaders(final ArrayList<ListView.FixedViewInfo> headers, final ListView listView) {
-        ListView.FixedViewInfo headerView = createFixedViewInfo(listView, getLayoutInflater(), R.layout.manage_category_list_header);
-        Button addButton = (Button) headerView.view.findViewById(R.id.manage_category_add_category_button);
+        setContentView(R.layout.manage_category);
+        Button addButton = (Button) findViewById(R.id.manage_category_add_category_button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(final View view) {
                 ManageCategoryActivity.this.showDialog(CREATE_DIALOG);
             }
         });
 
-        headers.add(headerView);
+        ListView listView = (ListView) findViewById(R.id.manage_category_category_list);
+//        ListAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, new String[] {"one", "two", "three"});
+        Cursor cursor = getContentResolver().query(WambalContentProvider.CONTENT_URI, new String[]{WambalContentProvider.Category._ID, WambalContentProvider.Category.NAME}, null, null, null);
+        startManagingCursor(cursor);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor,
+                new String[]{WambalContentProvider.Category.NAME}, new int[]{android.R.id.text1});
+        listView.setAdapter(adapter);
     }
 
     private ListView.FixedViewInfo createFixedViewInfo(ListView listView, final LayoutInflater inflater, final int layout) {
