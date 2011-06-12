6a2a31d1ccae99df4ef540ba1f6658ec4f6f4c80
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-06-12 19:30:56 +1000

    data now loads on the category screen. Still broken.

--- src/main/java/com/wambal/activity/ManageCategoryActivity.java
+++ src/main/java/com/wambal/activity/ManageCategoryActivity.java
@@ -4,34 +4,65 @@
  */
 package com.wambal.activity;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ListActivity;
 import android.content.ContentValues;
 import android.content.DialogInterface;
-import android.net.Uri;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 import com.wambal.R;
 import com.wambal.cp.WambalContentProvider;
 
-public final class ManageCategoryActivity extends Activity {
+import java.util.ArrayList;
+
+public final class ManageCategoryActivity extends ListActivity {
 
     private static final int CREATE_DIALOG = 1;
 
     @Override protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.manage_category);
-        Button addButton = (Button) findViewById(R.id.manage_category_add_category_button);
+        ArrayList<ListView.FixedViewInfo> footers = new ArrayList<ListView.FixedViewInfo>();
+        ArrayList<ListView.FixedViewInfo> headers = new ArrayList<ListView.FixedViewInfo>();
+
+        addHeaders(headers, getListView());
+        Cursor cursor = getContentResolver().query(WambalContentProvider.CONTENT_URI, new String[]{WambalContentProvider.Category._ID, WambalContentProvider.Category.NAME}, null, null, null);
+        startManagingCursor(cursor);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor,
+                new String[]{WambalContentProvider.Category.NAME}, new int[]{android.R.id.text1});
+        HeaderViewListAdapter headerAdapter = new HeaderViewListAdapter(headers, footers, adapter);
+        setListAdapter(headerAdapter);
+    }
+
+    private void addHeaders(final ArrayList<ListView.FixedViewInfo> headers, final ListView listView) {
+        ListView.FixedViewInfo headerView = createFixedViewInfo(listView, getLayoutInflater(), R.layout.manage_category_list_header);
+        Button addButton = (Button) headerView.view.findViewById(R.id.manage_category_add_category_button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(final View view) {
-                showDialog(CREATE_DIALOG);
+                ManageCategoryActivity.this.showDialog(CREATE_DIALOG);
             }
         });
+
+        headers.add(headerView);
     }
+
+    private ListView.FixedViewInfo createFixedViewInfo(ListView listView, final LayoutInflater inflater, final int layout) {
+        ListView.FixedViewInfo fvi= listView.new FixedViewInfo();
+        fvi.data = null;
+        fvi.isSelectable = false;
+        fvi.view = inflater.inflate(layout, null);
+        return fvi;
+    }
+
 
     @Override protected Dialog onCreateDialog(final int id) {
         switch (id) {
